b39ea66a261c7957c9b8181aca1072b0e8419396
panauhaij <pan18176793056@gmail.com>
2024-10-02 14:52:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "林的ChatGPT",
+  description: "林的ChatGPT网页",
   appleWebApp: {
     title: "NextChat",
     statusBarStyle: "default",
